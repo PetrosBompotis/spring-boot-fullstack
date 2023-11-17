d115500ae231f42d44b200a1b600d2b8c05fbd31
petrosb <petrosbompotis@gmail.com>
2023-11-17 22:19:08 +0200

    add line

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -28,6 +28,7 @@ public class Main {
             String firstName = name.firstName();
             String lastName = name.lastName();
 
+
             Customer customer = new Customer(random.nextInt(16,99),
                     firstName + " " + lastName,
                     firstName.toLowerCase() + "." + lastName.toLowerCase() + "amigoscode.com"
